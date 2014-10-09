95a90b46b697c5f14a92fcd21572e257e8de1a53
Aleksandr Nikitin <a.nikitin@data357.com>
2014-10-09 09:34:55 +0300

    Slider

--- js/index.js
+++ js/index.js
@@ -3,10 +3,7 @@
  */
 (function(){
 	var classNames = {
-		toysiSliderContent: 'toysi-slider-content',
-		toysiSliderPages: 'toysi-slider-pages',
-
-		active: 'active'
+		toysiSliderContainer: 'toysi-slider-container',
 	};
 
 	var ids = {
@@ -14,41 +11,12 @@
 
 	var selectors = ts.buildSelectors(classNames, ids);
 
-	var timerListSlide,
-		intervalSlider = 10000; // ms
-
-	var initializationSlider = function(){
-		var countSlides = $(selectors.toysiSliderContent).children().length;
-		$(selectors.toysiSliderPages).html('');
-		for (var i=0; i < countSlides; i++) {
-			$(selectors.toysiSliderPages).append($('<li/>'));
-		}
-		$(selectors.toysiSliderPages).find('li:first').addClass(classNames.active);
-
-		timerListSlide = setInterval(function(){
-			moveSlide();
-		}, intervalSlider);
-
-	};
-
-	var moveSlide = function() {
-		var $slider = $(selectors.toysiSliderContent),
-			$pages = $(selectors.toysiSliderPages),
-			sliderSpeed = 1000,
-			itemsTotal = $(selectors.toysiSliderPages).children().length;
-		$slider.children(':last').after($slider.children().slice(0, 1).clone(true));
-		$pages.children(':first').before($pages.children().slice(itemsTotal - 1, itemsTotal).clone(true));
-		$slider.children().slice(1, 2).animate({left: -735}, {queue: false, duration: sliderSpeed, complete: function() {
-			$slider.children().slice(1, 2).css({left: 0});
-		}});
-		$slider.children().slice(0, 1).animate({left: -735}, {queue: false, duration: sliderSpeed, complete: function() {
-			$slider.children().slice(0, 1).remove();
-		}});
-		$pages.children().slice(itemsTotal, itemsTotal + 1).remove();
-	};
-
 	$(function(){
-		initializationSlider();
+		$.getScript('js/slider.js').done(function(){
+			$(selectors.toysiSliderContainer).slider();
+		}).fail(function(){
+			console.log('Fail load file slider.js');
+		});
 	});
 
 })();
